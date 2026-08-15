7cc61ac092a4aa7d6e31a4dac385150928210371
NikitaBestfator <nbestfator@gmail.com>
2026-08-15 13:54:52 +0300

    MVP калькулятор WPF

---
+++ NikBestCalculator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NikBestCalculator;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
